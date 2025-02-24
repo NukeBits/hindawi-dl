c4b6dd9ea063b8464eed263b120e4e053d3bb7ec
NukeBits <salaho.ho@proton.me>
2025-02-24 11:14:59 +0000

    test(scrapers): test max_pages & books_num

--- testlib/scrapers/all_books_page.py
+++ testlib/scrapers/all_books_page.py
@@ -1,4 +1,4 @@
-# test scrapers/all_books_page,py
+# test scrapers/all_books_page.py
 
 from requests            import get as GET
 from hindawi_dl.scrapers import AllBooksPage
@@ -7,16 +7,30 @@ from hindawi_dl.utils    import Book
 
 
 
-html = GET('https://www.hindawi.org/books/').content
+html = GET('https://www.hindawi.org/books/categories/literary.criticism/').content
 
 
 class TestAllBookPage(TestCase):
     def test_books(self):
         books = list(AllBooksPage(html).books())
-        if self.assertTrue(len(books) > 0, "0 book was loaded"):
-        
-            bk = books[0]
-            self.assertEqual(type(bk), Book)
 
+        self.assertTrue(len(books) > 0, "0 book was loaded")
+        self.assertEqual(type(books[0]), Book)
+
+
+
+    def test_pages(self):
+        max_pages = AllBooksPage(html).max_pages
+
+        self.assertEqual(type(max_pages), int)
+        self.assertTrue(max_pages>0)
+
+
+
+    def test_books_num(self):
+        books_num = AllBooksPage(html).books_num
+
+        self.assertEqual(type(books_num), int)
+        self.assertTrue(books_num>0) 
 
 
